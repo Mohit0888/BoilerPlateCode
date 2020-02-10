d160f433500b9415608dbbebacd0d9aa73b00ed7
Mohit0888 <Mohit.solanki@thinkfirstservices.com>
2020-02-10 16:20:08 +0530

    Multiple Environment Setting

---
+++ boilerPlate/settings/production.py
@@ -0,0 +1,37 @@
+from .base import *
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['127.0.0.1']
+
+
+# Password validation
+# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'school',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST' : 'localhost'
+    }
+}
--- manage.py
+++ manage.py
@@ -5,7 +5,8 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'boilerPlate.settings')
+    #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'boilerPlate.settings.production')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'boilerPlate.settings.development')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
